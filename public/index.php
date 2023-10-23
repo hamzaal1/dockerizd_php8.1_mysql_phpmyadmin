949028d184f8519724219620459625c6d59ada52
hamza <hamzademnati21@gmail.com>
2023-10-23 17:27:15 +0100

    dockerized php 8.1 with mysql and phpmyadmin using docker-compose

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $servername = "db";
+    $username = "root";
+    $password = "root";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=php_mvc", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
+
+
+<h1>Hello World</h1>
+
+</body>
+</html>
